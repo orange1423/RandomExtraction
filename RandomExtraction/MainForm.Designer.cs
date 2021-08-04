a64432da206e9b04fd58b28c8322973d14b96f6f
orange <orange@icedeer.net>
2021-08-04 18:03:16 +0800

    first commit

---
+++ RandomExtraction/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RandomExtraction
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Extraction = new System.Windows.Forms.TextBox();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.textBox_hitokoto = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox_ = new System.Windows.Forms.PictureBox();
+            this.pictureBox_MessageForm = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_MessageForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_Extraction
+            // 
+            this.textBox_Extraction.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_Extraction.Font = new System.Drawing.Font("方正启功行楷 简", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_Extraction.Location = new System.Drawing.Point(500, 371);
+            this.textBox_Extraction.Name = "textBox_Extraction";
+            this.textBox_Extraction.ReadOnly = true;
+            this.textBox_Extraction.Size = new System.Drawing.Size(600, 55);
+            this.textBox_Extraction.TabIndex = 0;
+            this.textBox_Extraction.TabStop = false;
+            this.textBox_Extraction.Text = "咕咕咕";
+            this.textBox_Extraction.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox_Extraction.DoubleClick += new System.EventHandler(this.MainForm_DoubleClick);
+            // 
+            // button_Start
+            // 
+            this.button_Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Start.Location = new System.Drawing.Point(740, 461);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(120, 50);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // textBox_hitokoto
+            // 
+            this.textBox_hitokoto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_hitokoto.Font = new System.Drawing.Font("方正启功行楷 简", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_hitokoto.Location = new System.Drawing.Point(50, 60);
+            this.textBox_hitokoto.Multiline = true;
+            this.textBox_hitokoto.Name = "textBox_hitokoto";
+            this.textBox_hitokoto.ReadOnly = true;
+            this.textBox_hitokoto.Size = new System.Drawing.Size(1500, 120);
+            this.textBox_hitokoto.TabIndex = 1;
+            this.textBox_hitokoto.TabStop = false;
+            // 
+            // pictureBox_
+            // 
+            this.pictureBox_.Image = global::RandomExtraction.Properties.Resources.竹子;
+            this.pictureBox_.Location = new System.Drawing.Point(35, 550);
+            this.pictureBox_.Name = "pictureBox_";
+            this.pictureBox_.Size = new System.Drawing.Size(300, 350);
+            this.pictureBox_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_.TabIndex = 3;
+            this.pictureBox_.TabStop = false;
+            // 
+            // pictureBox_MessageForm
+            // 
+            this.pictureBox_MessageForm.Image = global::RandomExtraction.Properties.Resources.花草;
+            this.pictureBox_MessageForm.Location = new System.Drawing.Point(1280, 700);
+            this.pictureBox_MessageForm.Name = "pictureBox_MessageForm";
+            this.pictureBox_MessageForm.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox_MessageForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_MessageForm.TabIndex = 2;
+            this.pictureBox_MessageForm.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.button_Start;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 882);
+            this.Controls.Add(this.pictureBox_);
+            this.Controls.Add(this.pictureBox_MessageForm);
+            this.Controls.Add(this.textBox_hitokoto);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.textBox_Extraction);
+            this.Font = new System.Drawing.Font("方正启功行楷 简", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "MainForm";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DoubleClick += new System.EventHandler(this.MainForm_DoubleClick);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_MessageForm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Extraction;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.TextBox textBox_hitokoto;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox_MessageForm;
+        private System.Windows.Forms.PictureBox pictureBox_;
+    }
+}
+
---
+++ RandomExtraction/MainForm.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RandomExtraction
+{
+    public partial class MainForm : Form
+    {
+        bool ignoreweight = false;
+        bool nohitokoto = false;
+        bool nofrom = false;
+        bool isStart = false;
+        string extractionType = "all";
+        string listFile = "list.csv";
+        string ignoreListFile = "";
+        string mustListFile = "";
+        string hitokotoFiles = "";
+        string hitokotoUrl = "https://v1.hitokoto.cn/";
+        string hitokotoParameters = "?c=i&c=d";
+        List<string> list = new List<string>();
+        List<string> ignoreList = new List<string>();
+        List<string> mustList = new List<string>();
+        List<string> excludeList = new List<string>();
+        Thread thread;
+        public MainForm(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                if (arg == "--ignoreweight")
+                {
+                    ignoreweight = true;
+                }
+                else if (arg.StartsWith("--list"))
+                {
+                    listFile = arg.Split('=')[1];
+                }
+                else if (arg.StartsWith("--ignorelist"))
+                {
+                    ignoreListFile = arg.Split('=')[1];
+                }
+                else if (arg.StartsWith("--mustList"))
+                {
+                    mustListFile = arg.Split('=')[1];
+                }
+                else if (arg.StartsWith("--type"))
+                {
+                    extractionType = arg.Split('=')[1];
+                }
+                else if (arg == "--nohitokoto")
+                {
+                    nohitokoto = true;
+                }
+                else if (arg == "--nofrom")
+                {
+                    nofrom = true;
+                }
+                else if (arg.StartsWith("--hitokotourl"))
+                {
+                    hitokotoUrl = arg.Split('=')[1];
+                }
+                else if (arg.StartsWith("--hitokotoparameters"))
+                {
+                    hitokotoParameters = arg.Split('=')[1];
+                }
+                else if(arg.StartsWith("--hitokotofiles"))
+                {
+                    hitokotoFiles = arg.Split('=')[1];
+                }
+                else if(arg.StartsWith("--exclude"))
+                {
+                    excludeList.AddRange(arg.Split('=')[1].Split(','));
+                }
+                else if (arg.StartsWith("--ignore"))
+                {
+                    ignoreList.AddRange(arg.Split('=')[1].Split(','));
+                }
+                else if (arg.StartsWith("--must"))
+                {
+                    mustList.AddRange(arg.Split('=')[1].Split(','));
+                }
+                else
+                {
+                    listFile = arg;
+                }
+            }
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            prepareLists();
+            if(!nohitokoto)
+            {
+                refreshHitokoto();
+            }
+            changeColor();
+        }
+
+        private void prepareLists()
+        {
+            list.Clear();
+            StreamReader reader = new StreamReader(listFile, Encoding.UTF8);
+            bool flag = true;
+            while (flag)
+            {
+                var listText = reader.ReadLine();
+                if (listText == null)
+                {
+                    flag = false;
+                    break;
+                }
+                string[] texts = listText.Split(',');
+                if(excludeList.Exists(t => t == texts[0]))
+                {
+                    continue;
+                }
+                if(extractionType != "all" && extractionType != texts[1].Trim())
+                {
+                    continue;
+                }
+                if (ignoreweight)
+                {
+                    list.Add(texts[0].Trim());
+                }
+                else
+                {
+                    for (int i = int.Parse(texts[2]); i > 0; i--)
+                    {
+                        list.Add(texts[0].Trim());
+                    }
+                }
+            }
+            reader.Close();
+            if (ignoreListFile != "")
+            {
+                reader = new StreamReader(ignoreListFile, Encoding.UTF8);
+                flag = true;
+                while (flag)
+                {
+                    string listText = reader.ReadLine();
+                    if (listText == null)
+                    {
+                        flag = false;
+                        break;
+                    }
+                    if(!ignoreList.Exists(t => t == listText.Trim()))
+                    ignoreList.Add(listText.Trim());
+                }
+                reader.Close();
+            }
+            if(mustListFile != "")
+            {
+                reader = new StreamReader(mustListFile, Encoding.UTF8);
+                flag = true;
+                while (flag)
+                {
+                    string listText = reader.ReadLine();
+                    if (listText == null)
+                    {
+                        flag = false;
+                        break;
+                    }
+                    if(!mustList.Exists(t => t == listText.Trim()))
+                    {
+                        mustList.Add(listText.Trim());
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            if(isStart)
+            {
+                isStart = false;
+                button_Start.Text = "Start";
+            }
+            else
+            {
+                isStart = true;
+                thread = new Thread(new ThreadStart(Extract));
+                thread.Start();
+                button_Start.Text = "Stop";
+            }
+        }
+
+        private void Extract()
+        {
+            Random random = new Random();
+            while (isStart)
+            {
+                int index = random.Next(0, list.Count - 1);
+                textBox_Extraction.Text = list[index];
+                Thread.Sleep(25);
+             }
+             if(mustList.Count > 0)
+            {
+                if(list.Exists(t => t == mustList[0]))
+                {
+                    textBox_Extraction.Text = mustList[0];
+                }
+                mustList.RemoveAt(0);
+            }
+            else
+            {
+                while (ignoreList.Exists(t => t == textBox_Extraction.Text))
+                {
+                    int index = random.Next(0, list.Count - 1);
+                    textBox_Extraction.Text = list[index];
+                }
+            }
+            if (!nohitokoto)
+            {
+                refreshHitokoto();
+            }
+        }
+
+        private void refreshHitokoto()
+        {
+            JArray hitokotoArray = new JArray();
+            foreach (string hitokotoFile in hitokotoFiles.Split('&'))
+            {
+                if (File.Exists(hitokotoFile))
+                {
+                    StreamReader reader = new StreamReader(hitokotoFile);
+                 
+                    hitokotoArray.Merge(JArray.Parse(reader.ReadToEnd()));
+                }
+            }
+            JObject hitokotoJson;
+            if (hitokotoArray.Count > 0)
+            {
+                Random random = new Random();
+                hitokotoJson = JObject.Parse(hitokotoArray[random.Next(0, hitokotoArray.Count)].ToString());
+            }
+            else
+            {
+                hitokotoJson = JObject.Parse(HttpGet(hitokotoUrl + hitokotoParameters, null));
+            }
+            string hitokotoText = "";
+            hitokotoText += hitokotoJson["hitokoto"].ToString().Trim();
+            if(!nofrom)
+            {
+                hitokotoText += "    ——";
+                hitokotoText += hitokotoJson["from_who"];
+                hitokotoText += "「" + hitokotoJson["from"] + "」";
+            }
+            textBox_hitokoto.Text = hitokotoText;
+        }
+
+        public string HttpGet(string Url, string contentType)
+        {
+            try
+            {
+                string retString = string.Empty;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Method = "GET";
+                request.ContentType = contentType;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(myResponseStream);
+                retString = streamReader.ReadToEnd();
+                streamReader.Close();
+                myResponseStream.Close();
+                return retString;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void MainForm_DoubleClick(object sender, EventArgs e)
+        {
+            if (extractionType == "all")
+            {
+                extractionType = "girl";
+            }
+            else if (extractionType == "girl")
+            {
+                extractionType = "boy";
+            }
+            else if (extractionType == "boy")
+            {
+                extractionType = "all";
+            }
+            changeColor();
+            prepareLists();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void changeColor()
+        {
+            Color color;
+            bool visible = false;
+
+            if (extractionType == "girl")
+            {
+                color = Color.FromArgb(152, 153, 255);
+            }
+            else if(extractionType == "boy")
+            {
+                color = Color.FromArgb(102, 204, 255);
+            }
+            else
+            {
+                color = SystemColors.Control;
+                visible = true;
+            }
+            this.BackColor = color;
+            textBox_Extraction.BackColor = color;
+            textBox_hitokoto.BackColor = color;
+            pictureBox_MessageForm.Visible = visible;
+            pictureBox_.Visible = visible;
+        }
+    }
+}
